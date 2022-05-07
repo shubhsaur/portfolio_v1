2d1e1b4380449842ab8ff91054900f8bd57ff632
Shubham Saurabh <shubhamsaurabh@outlook.com>
2022-05-07 23:21:22 +0530

    added projects, contacts and footer section

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ sideNavLinks.forEach((link) => {
 		this.classList.add("active");
 	});
 });
+
+// const texts = document.querySelectorAll(".project-card .text");
+// const projects = document.querySelectorAll(".project-card");
+
+// projects.forEach((project) => {
+// 	project.addEventListener("mouseover", function () {
+// 		texts.forEach((txt) => (txt.style.display = "none"));
+// 		this.style.display = "block";
+// 	});
+// });
